442c49bf99cef05fba081d636b7d7d0ac646b84a
Mahesh Shelke <shelkemahesh2893@gmail.com>
2024-07-05 18:43:22 +0530

    adding BasePage data member 'WebDriver driver' with package private access specifier and adding constructor to initialize this webdriver instance

--- src/main/java/project/squads/BasePage.java
+++ src/main/java/project/squads/BasePage.java
@@ -1,5 +1,13 @@
 package project.squads;
 
+import org.openqa.selenium.WebDriver;
+
 public class BasePage {
+
+	WebDriver driver;
+
+	public BasePage(WebDriver driverFromChildClass) {
+		this.driver = driverFromChildClass;
+	}
 
 }
