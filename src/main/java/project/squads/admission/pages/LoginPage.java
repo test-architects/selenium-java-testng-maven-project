999d8494459822ad5d3332805889ab688ee35c3b
Mahesh Shelke <shelkemahesh2893@gmail.com>
2024-07-06 22:03:16 +0530

    adding LoginPage skeleton

---
+++ src/main/java/project/squads/admission/pages/LoginPage.java
@@ -0,0 +1,11 @@
+package project.squads.admission.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import project.squads.BasePage;
+
+public class LoginPage extends BasePage {
+	public LoginPage(WebDriver driver){
+		super(driver);
+	}
+}
