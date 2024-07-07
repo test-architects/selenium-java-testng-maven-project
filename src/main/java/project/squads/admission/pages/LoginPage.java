685ec67b9b4d663d9028ba182634d5fb613233eb
Mahesh Shelke <shelkemahesh2893@gmail.com>
2024-07-07 16:10:04 +0530

    about page object model and adding LoginPage

--- src/main/java/project/squads/admission/pages/LoginPage.java
+++ src/main/java/project/squads/admission/pages/LoginPage.java
@@ -1,11 +1,16 @@
 package project.squads.admission.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 import project.squads.BasePage;
 
 public class LoginPage extends BasePage {
-	public LoginPage(WebDriver driver){
+
+	public LoginPage(WebDriver driver) {
 		super(driver);
+		PageFactory.initElements(driver, this);
 	}
+	
+	
 }
