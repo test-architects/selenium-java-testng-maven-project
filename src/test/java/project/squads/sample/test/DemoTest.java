fcd0cc6e56559417d5f60889b7e6b69b24a8b34a
Mahesh Shelke <shelkemahesh2893@gmail.com>
2024-07-07 19:20:52 +0530

    updating testng version to 7.9.0 and adding demo or sample test

---
+++ src/test/java/project/squads/sample/test/DemoTest.java
@@ -0,0 +1,32 @@
+package project.squads.sample.test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import project.squads.BaseTest;
+
+public class DemoTest extends BaseTest {
+
+	WebDriver driver;
+
+	@BeforeClass
+	public void setUp() {
+		setDriver();
+		driver = getWebDriver();
+	}
+
+	@Test
+	public void getURL() {
+		driver.navigate().to("https://maheshgit9637.github.io/loginForm/login.html");
+	}
+
+	@AfterClass
+	public void tearDown() {
+		removeDriver();
+	}
+
+}
