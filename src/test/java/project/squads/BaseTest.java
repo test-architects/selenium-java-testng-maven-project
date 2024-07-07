a2aac294af04f822a56a136a3c73ad3cc69b2a0a
Mahesh Shelke <shelkemahesh2893@gmail.com>
2024-07-07 17:08:41 +0530

    adding base test with threadlocal logic

--- src/test/java/project/squads/BaseTest.java
+++ src/test/java/project/squads/BaseTest.java
@@ -1,5 +1,25 @@
 package project.squads;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
+
+	
+	protected ThreadLocal<WebDriver> threadLocalDriver = new ThreadLocal<WebDriver>();
+	// its thread safe, webdriver instance will be unique for each test, it will
+	// assist in parellel execution
+
+	public WebDriver getWebDriver() {
+		return threadLocalDriver.get();
+	}
+	
+	public void setDriver() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		threadLocalDriver.set(driver);
+	}
 
 }
