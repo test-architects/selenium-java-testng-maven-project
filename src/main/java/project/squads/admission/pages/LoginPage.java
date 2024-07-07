dfb592036e357764e53ff240b2acca1fb56128a8
Mahesh Shelke <shelkemahesh2893@gmail.com>
2024-07-07 16:31:08 +0530

    pushing login page to remote

--- src/main/java/project/squads/admission/pages/LoginPage.java
+++ src/main/java/project/squads/admission/pages/LoginPage.java
@@ -1,6 +1,9 @@
 package project.squads.admission.pages;
 
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import project.squads.BasePage;
@@ -11,6 +14,56 @@ public class LoginPage extends BasePage {
 		super(driver);
 		PageFactory.initElements(driver, this);
 	}
-	
-	
+
+	@FindBy(id = "username")
+	private WebElement usernameTextBox;
+
+	@FindBy(name = "password")
+	private WebElement passwordTextBox;
+
+	@FindBy(xpath = "//input[@value='Login']")
+	private WebElement loginButton;
+
+	public boolean isUserOnLoginPage() {
+		waitForElementToBeVisible(usernameTextBox, 5);
+		if (driver.getTitle() == "Login Page") {
+			System.out.println("user is on login page");
+			return true;
+		} else {
+			System.out.println("user is not on login page");
+			return false;
+		}
+	}
+
+	public void enterUsername(String username) {
+		waitForElementToBeVisible(usernameTextBox, 5);
+		usernameTextBox.clear();
+		usernameTextBox.sendKeys(username);
+		System.out.println("succesfully entered the username as :" + username);
+	}
+
+	public void enterPassword(String password) {
+		waitForElementToBeVisible(passwordTextBox, 5);
+		passwordTextBox.clear();
+		passwordTextBox.sendKeys(password);
+		System.out.println("succesfully entered the password as :" + password);
+
+	}
+
+	public void clickLoginButton() {
+		loginButton.click();
+	}
+
+	public boolean isAlertPresent() {
+		boolean flag = false;
+		try {
+			driver.switchTo().alert();
+			flag = true;
+			driver.switchTo().alert().accept();
+		} catch (NoAlertPresentException e) {
+			flag = false;
+		}
+		return flag;
+	}
+
 }
