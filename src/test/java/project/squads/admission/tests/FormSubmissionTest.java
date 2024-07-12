1ed450358f51048782a48b80e218a4ea0a0f2632
Mahesh Shelke <shelkemahesh2893@gmail.com>
2024-07-12 17:10:33 +0530

    pushing sample test in FormSubmissiontest

--- src/test/java/project/squads/admission/tests/FormSubmissionTest.java
+++ src/test/java/project/squads/admission/tests/FormSubmissionTest.java
@@ -1,7 +1,30 @@
 package project.squads.admission.tests;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 import project.squads.BaseTest;
+import project.squads.admission.pages.loginpage.LoginPage;
 
 public class FormSubmissionTest extends BaseTest {
+
+	LoginPage lp;;
+
+	@BeforeClass
+	public void setUpBroweser() {
+		setDriver();
+		lp = new LoginPage(getWebDriver());
+	}
+
+	@AfterClass
+	public void quitBrowserSesion() {
+		removeDriver();
+	}
+
+	@Test
+	public void TestA() {
+		System.out.println("sample test in FormSubmissionTest");
+	}
 
 }
